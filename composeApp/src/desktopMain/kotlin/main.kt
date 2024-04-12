2be1c946a5d25a4a7358b417b0b9decee7224a3f
Giuseppe Barbieri <elect86@gmail.com>
2024-04-12 12:10:32 +0200

    encoded connection status in tray icon color

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,15 +1,21 @@
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameMillis
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.isTraySupported
 import com.zoffcc.applications.jninotifications.NTFYActivity.jninotifications_loadjni
 import com.zoffcc.applications.jninotifications.NTFYActivity.jninotifications_notify
-import easnx.composeapp.generated.resources.Res
-import easnx.composeapp.generated.resources.vpn
 import org.ietf.tools.getTOTPCode
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import java.awt.Toolkit.getDefaultToolkit
 import java.awt.datatransfer.StringSelection
 import java.io.File
+import java.net.Inet4Address
 import java.nio.file.Paths
 
 val iconPath = File("./vpn-network-icon.webp").absolutePath
@@ -21,12 +27,28 @@ val totp
 const val alreadyRunning = "Another session of SNX is already running, aborting...\n"
 fun notify(message: String, title: String = "snx") = jninotifications_notify("snx", title, message, iconPath)
 
-@OptIn(ExperimentalResourceApi::class)
 fun main() {
+    val pingPeriodMs = 1_000
+    check(isTraySupported)
+
     val pwd = System.getenv("HZDR_PASSWORD")
     jninotifications_loadjni(Paths.get("").toAbsolutePath().toString())
     application {
-        Tray(icon = painterResource(Res.drawable.vpn),
+
+        var time = 0L
+        val dnsReachable = remember { mutableStateOf(false) }
+
+        LaunchedEffect(Unit) {
+            while (true)
+                withFrameMillis {
+                    if (it - time > pingPeriodMs) {
+                        time = it
+                        dnsReachable.value = Inet4Address.getByName(secondaryDNS).isReachable(500)
+                    }
+                }
+        }
+
+        Tray(icon = if (dnsReachable.value) green else red,
              menu = {
                  Item("Connect", onClick = {
                      val snx = ProcessBuilder("snx").start()
@@ -36,13 +58,14 @@ fun main() {
                      } else {
                          ProcessBuilder("byobu-tmux", "new-session", "-d", "echo $pwd$totp | nohup snx -s cp.hzdr.de -u $user").start()
                          val output = ProcessBuilder("snx").start().output
-                         val connected = output == alreadyRunning || output == "Failed to init terminal!\n"
-                         //                     println(output)
-                         notify(if (connected) "connected" else "error")
+//                         val connected = output == alreadyRunning || output == "Failed to init terminal!\n"
+                         println(output)
+                         notify("trying to connect..")
                      }
                  })
                  Item("Disconnect", onClick = {
                      val output = ProcessBuilder("snx", "-d").start().output
+                     dnsReachable.value = false
                      // we get the same string begin, either successful or not
                      val title = "SNX - Disconnecting...\n"
                      notify(output.substringAfter(title), title)
@@ -51,4 +74,13 @@ fun main() {
                  Item("Exit", onClick = ::exitApplication)
              })
     }
+}
+
+val red = TrayIcon(Color.Red)
+val green = TrayIcon(Color.Green)
+const val secondaryDNS = "149.220.4.2"
+
+class TrayIcon(val color: Color) : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+    override fun DrawScope.onDraw() = drawRect(color)
 }
